cf1dae18ff5a065db345922c8eab9e4efb9644b7
DeepakPoonia0000 <dpoonia0000@gmail.com>
2024-11-12 19:34:04 +0530

    nav bar

--- model/EmeregencyRequestSchema.js
+++ model/EmeregencyRequestSchema.js
@@ -5,6 +5,10 @@ const mongoose = require('mongoose')
 const emergencySchema = new mongoose.Schema({
     requestorId: String,
     name: String,
+    city:String,
+    hospitalName:{
+        type:String
+    },
     otp: Number,
     otpExpiry: Date,
     status: {
